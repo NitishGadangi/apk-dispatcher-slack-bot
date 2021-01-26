1e4618adb8afb0894e6581bd26856f697ba2b25b
Nitish Gadangi <nitishgadangi@gmail.com>
2021-01-27 01:36:23 +0530

    fix message

--- routes/android.js
+++ routes/android.js
@@ -10,12 +10,8 @@ const trigger_api = process.env.TRIGGER_API;
 
 function generateSuccessReply(user_id, ticket, ref){
     return {
-        "response_type": "ephemeral",
         "replace_original": false,
-        "text": {
-            "type": "mrkdwn",
-            "text": `${user_id} Your request is submitted. Relax :coffee: ! while I build the Apk for you!\n>This usually takes 4-5 minutes. I *will ping you* once its done.\n>Your ticket - ${ticket} \n>Branch Selected - *${ticket}*`
-        }
+        "text": `${user_id} Your request is submitted. Relax :coffee: ! while I build the Apk for you!\n >This usually takes 4-5 minutes. I *will ping you* once its done.\n >Your ticket - ${ticket} \n >Branch Selected - *${ticket}*`
       }
 }
 
