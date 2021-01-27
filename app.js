daf45d11d034c5a32fc5f9d31f19dea607dd3cd3
Nitish Gadangi <nitishgadangi@gmail.com>
2021-01-27 13:52:30 +0530

    remove slack route

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var slackRouter = require('./routes/slack');
 var androidRouter = require('./routes/android');
 
 var app = express();
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/slack', slackRouter);
 app.use('/android', androidRouter);
 
 // catch 404 and forward to error handler
